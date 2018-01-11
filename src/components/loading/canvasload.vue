1f234a54508f6023dc2373985c95ee61be1d3d7e
L6zt <824561745@qq.com>
2018-01-11 21:08:05 +0800

    xxx

--- src/components/loading/canvasload.vue
+++ src/components/loading/canvasload.vue
@@ -41,18 +41,33 @@
 	        draw (deg) {
 		        // 初始化
 		        const {$options: {ctx}} = this
+		        const endAngle = Math.PI * 1.5
+                const times = parseInt(deg / endAngle)
+                const k = parseInt(times / 2) + 1
+                const startAngle = deg % endAngle
                 ctx.clearRect(0, 0, 100, 100)
 		        ctx.save()
 		        ctx.translate(50, 50)
                 ctx.rotate(deg)
-		        ctx.beginPath()
-		        ctx.fillStyle = '#333'
-		        ctx.arc(0, 0, 25, 0 ,Math.PI, true)
-		        ctx.fill()
-		        ctx.beginPath()
-		        ctx.fillStyle = '#fff'
-		        ctx.arc(0, 0, 20, 0, Math.PI, true)
-                ctx.fill()
+                ctx.beginPath()
+		        ctx.lineWidth = 5
+		        ctx.strokeStyle = 'black'
+                 // 0 1.5 1.5 1.5 3 3
+                // 1
+//		        ctx.arc(0, 0, 25, startAngle , endAngle, false) // wu
+//		        ctx.arc(0, 0, 25, endAngle ,  endAngle + startAngle ) // you
+//		        // 2
+//		        ctx.arc(0,0, 25, endAngle + startAngle , 2 * endAngle)// wu
+//		        ctx.arc(0,0, 25, 2 * endAngle , 2 * endAngle  + startAngle )// you
+//		        // 3
+//		        ctx.arc(0,0, 25, 2 * endAngle + startAngle , 3 * endAngle)// wu
+                if (times % 2 === 0) {
+	                ctx.arc(0, 0, 25, startAngle + (k - 1) * endAngle, k * endAngle, false) // wu
+                } else {
+	                ctx.arc(0, 0, 25, k * endAngle ,  k * endAngle + startAngle, false) // you
+                }
+		        ctx.stroke()
+                ctx.closePath()
                 ctx.restore()
 	        }
         }
