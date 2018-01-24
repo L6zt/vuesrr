0d7383f97ce4f42c91a3855500d56f4b6f81ffad
L6zt <824561745@qq.com>
2018-01-24 11:12:54 +0800

    add silder init, wait complete

--- src/components/slide/slide.vue
+++ src/components/slide/slide.vue
@@ -1,5 +1,57 @@
 <template>
-    <div class="jc-slide-comp">
-
+    <div class="jc-slide-comp" :style="{width: `${width}`}">
+        <div class="jc-slide-container" :style="{width: `${width}`}">
+            <i class="slide-btn"></i>
+            <section class="slide-line"></section>
+        </div>
     </div>
 </template>
+<script>
+    export default {
+        props: {
+        	width: {
+        		type: String,
+                default: '200px'
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+<style>
+    @component-namespace jc {
+        @component slide-comp {
+            position: relative;
+            .jc-slide-container {
+                position: relative;
+                height: 30px;
+                padding: 12px 0;
+                box-sizing: border-box;
+                display: inline-block;
+               .slide-btn {
+                position: absolute;
+                display: block;
+                left: 0;
+                top: 0;
+                bottom: 0;
+                margin: auto;
+                width: 20px;
+                height: 20px;
+                border-radius: 50%;
+                background: #fff;
+                box-shadow: 0 0 3px #333;
+                z-index: 999;
+                cursor: pointer;
+            }
+               .slide-line {
+                position: relative;
+                height: 100%;
+                width: 100%;
+                background: #ccc;
+                border-radius: 5px;
+            }
+            }
+        }
+    }
+</style>
